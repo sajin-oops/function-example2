1608cffb76acd78a3efd0ef7e8523d29143e7523
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-09 22:40:13 +0530

    Add files via upload

---
+++ function practice.py
@@ -0,0 +1,31 @@
+# return = statement used to end a function and send a result back to the caller
+
+def add(x,y):
+  z = x + y
+  return z
+
+def sub(x,y):
+  z = x - y
+  return z
+
+def mul(x,y):
+  z = x * y
+  return z
+
+def div(x,y):
+  z = x / y
+  return z
+
+print(add(1,2))
+print(sub(1,2))
+print(mul(1,2))
+print(div(1,2))
+# - O/P
+'''
+
+3
+-1
+2
+0.5
+
+'''
